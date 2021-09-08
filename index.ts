1ec3c74952e398cbf7c711a699bbc885150f6655
r-tomiyama <r.tomiyama@opt.ne.jp>
2021-09-08 23:10:56 +0900

    add: socket.io

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import socketio from "socket.io";
 
 const app: express.Express = express();
 
@@ -7,7 +9,22 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
+const server = http.createServer(app);
+const io: socketio.Server = new socketio.Server(server);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+io.on("connection", (socket: socketio.Socket) => {
+  console.log("connect");
+
+  let counter = 0;
+
+  socket.on("test", (data: { message: string }) => {
+    console.log(`type: ${typeof data}   data: ${data.message}`);
+    socket.emit("test2", { message: `server message ${counter++}` });
+  });
+});
+
+server.listen(3000, () => {
   console.log("Start on port 3000.");
 });
 
