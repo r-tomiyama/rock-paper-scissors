56c977a51927c37ee7e6f05f6debef725a377206
r-tomiyama <r.tomiyama@opt.ne.jp>
2021-10-06 22:43:01 +0900

    add: エラー時にルームをリセットする

--- index.ts
+++ index.ts
@@ -45,57 +45,77 @@ io.on("connection", (socket: socketio.Socket) => {
 
   let isJoined = false;
 
-  // TODO: 例外復帰
+  function resetRoom() {
+    players = [];
+    io.emit("reset-room", {
+      message: "エラーが発生したため、トップページに戻ります。",
+    });
+  }
 
   socket.on("join", () => {
     console.log("join申請");
 
-    if (players.length < 2) {
-      players.push({ id: userId });
-      isJoined = true;
-      console.log(`join成功しました。${players.length}人目の参加者です。`);
-      socket.emit("joined", { id: userId });
+    try {
+      if (players.length < 2) {
+        players.push({ id: userId });
+        isJoined = true;
+        console.log(`join成功しました。${players.length}人目の参加者です。`);
+        socket.emit("joined", { id: userId });
 
-      if (players.length === 2) {
-        io.emit("start");
+        if (players.length === 2) {
+          io.emit("start");
+        }
+      } else {
+        console.log("満員のため、joinできません。");
+        socket.emit("error", {
+          message:
+            "じゃんけん部屋は満室のため、参加できません。時間を空けてからまた参加しましょう。",
+        });
       }
-    } else {
-      console.log("満員のため、joinできません。");
-      socket.emit("error", {
-        message:
-          "じゃんけん部屋は満室のため、参加できません。時間を空けてからまた参加しましょう。",
-      });
+    } catch (e) {
+      resetRoom();
+      console.error(e);
     }
   });
 
   socket.on("choose", (data: { hand: Hand }) => {
     console.log(`${data.hand}が選択されました`);
 
-    const user = players[players.findIndex((player) => player.id === userId)];
-    user.hand = data.hand;
+    try {
+      const user = players[players.findIndex((player) => player.id === userId)];
+      user.hand = data.hand;
 
-    const opponent = players.filter((player) => player.id !== user.id)[0];
+      const opponent = players.filter((player) => player.id !== user.id)[0];
 
-    if (opponent.hand) {
-      const result = calc_result(user.hand, opponent.hand);
-      io.emit("done", {
-        isDraw: result === "draw",
-        winner: result === "win" ? user.id : opponent.id,
-        opponentHand: opponent.hand,
-      });
+      if (opponent.hand) {
+        const result = calc_result(user.hand, opponent.hand);
+        io.emit("done", {
+          isDraw: result === "draw",
+          winner: result === "win" ? user.id : opponent.id,
+          opponentHand: opponent.hand,
+        });
 
-      players.forEach((player) => (player.hand = undefined));
+        players.forEach((player) => (player.hand = undefined));
+      }
+    } catch (e) {
+      resetRoom();
+      console.error(e);
     }
   });
 
   socket.on("disconnect", () => {
     console.log("disconnect");
 
-    if (isJoined) {
-      players = players.filter((player) => player.id === userId);
-      console.log(`現在の参加者は${players.length}人です。`);
+    try {
+      if (isJoined) {
+        players = players.filter((player) => player.id === userId);
+        console.log(`現在の参加者は${players.length}人です。`);
 
-      io.emit("opponent-disconnect");
+        io.emit("opponent-disconnect");
+      }
+    } catch (e) {
+      resetRoom();
+      console.error(e);
     }
   });
 });
